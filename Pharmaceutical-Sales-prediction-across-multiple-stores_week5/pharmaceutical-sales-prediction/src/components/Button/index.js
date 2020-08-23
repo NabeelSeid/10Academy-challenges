66f7fb653c2b59ffd03205d5e2513be4d3f4a1ff
Nabil Seid <nabeelseid@gmail.com>
2020-08-23 14:01:54 +0300

    pharmaceutical sales demo webapp deleted

--- Pharmaceutical-Sales-prediction-across-multiple-stores_week5/pharmaceutical-sales-prediction/src/components/Button/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-// BUTTON COMPONENT
-export default ({ onClick, className = '', children }) => (
-  <button onClick={onClick} className={className} type="button">
-    {children}
-  </button>
-)
